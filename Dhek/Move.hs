bce3271608758f3269b960b372d9132f4a078525
YoEight <yo.eight@gmail.com>
2013-11-18 11:41:45 +0100

    New onMove layout

--- Dhek/Move.hs
+++ Dhek/Move.hs
@@ -19,122 +19,56 @@ import Debug.Trace
 
 onMove :: DhekProgram ()
 onMove = compile $ do
-    (x,y)   <- getPointer
-    oOpt    <- getOverRect
-    aOpt    <- getOverArea
-    sOpt    <- getSelection
-    eOpt'   <- getEvent
-    lOpt    <- getCollision
-    overlap <- isActive Overlap
-    bidon   <- getBidon
-    when bidon (setBidon False)
-
-    let eOpt = bool bidon Nothing eOpt'
-        onEvent   = isJust eOpt
-        selection = do
-             x0 <- use rectX
-             y0 <- use rectY
-             rectWidth  .= x - x0
-             rectHeight .= y - y0
-
-        event (Hold r (x0,y0))     =
-            Hold (translateRect (x-x0) (y-y0) r) (x,y)
-        event (Resize r (x0,y0) a) =
-            Resize (resizeRect (x-x0) (y-y0) a r) (x,y) a
-
-        eventD NORTH (Hold r (x0,y0)) =
-            Hold (translateRectX (x-x0) r) (x,y0)
-        eventD SOUTH (Hold r (x0,y0)) =
-            Hold (translateRectX (x-x0) r) (x,y0)
-        eventD WEST (Hold r (x0,y0)) =
-            Hold (translateRectY (y-y0) r) (x0,y)
-        eventD EAST (Hold r (x0,y0)) =
-            Hold (translateRectY (y-y0) r) (x0,y)
-        eventD _ e = event e
-
-        cOpt = fmap eventCursor eOpt <|>
+    oOpt <- getOverRect
+    aOpt <- getOverArea
+    eOpt <- getEvent
+    sOpt <- getSelection
+    traverse_ onSelection sOpt
+    traverse_ onEvent eOpt
+    let cOpt = fmap eventCursor eOpt <|>
                fmap areaCursor aOpt  <|>
                handCursor <$ oOpt
-
-        eOpt2 = fmap event eOpt
-        sOpt2 = fmap (execState selection) sOpt
-
-        onCollisionActivated = do
-            rs <- getRects
-            for_ (eOpt2 >>= eventGetRect) $ \l ->
-                for_ (intersection rs l) $ \(r, d) -> do
-                    let (rmin, rmax) = rectRange d r
-                        (delta, l1)  = replaceRect d l r
-                        (x1, y1)     =
-                            case d of
-                                NORTH -> (x, y-delta)
-                                WEST  -> (x-delta, y)
-                                SOUTH -> (x, y+delta)
-                                EAST  -> (x+delta, y)
-
-                        opp = oppositeDirection d
-                        de  = fromEdge opp (x1,y1) l1
-
-                    setCollision $ Just (x1,y1,de,rmin,rmax,d)
-                    setEventRect l1
-
-        prevCollision (x0,y0,de,rmin,rmax,d) = do
-            let delta =
-                    case d of
-                        NORTH -> y-y0
-                        SOUTH -> y0-y
-                        WEST  -> x-x0
-                        EAST  -> x0-x
-
-                doesCollides l =
-                    let lx = l ^. rectX
-                        ly = l ^. rectY
-                        lw = l ^. rectWidth
-                        lh = l ^. rectHeight in
-                    case d of
-                        NORTH -> rmin <= (lx+lw) && lx <= rmax
-                        SOUTH -> rmin <= (lx+lw) && lx <= rmax
-                        EAST  -> rmin <= (ly+lh) && ly <= rmax
-                        WEST  -> rmin <= (ly+lh) && ly <= rmax
-
-                correct NORTH (Hold r (px,py)) =
-                     Hold (r & rectY .~ y0+de) (px, y0+de)
-                correct SOUTH (Hold r (px, py)) =
-                    Hold (r & rectY .~ y0-de) (px, y0-de)
-                correct WEST (Hold r (px,py)) =
-                    Hold (r & rectX .~ x0+de) (x0+de, py)
-                correct EAST (Hold r (px,py)) =
-                    Hold (r & rectX .~ x0-de) (x0-de, py)
-                correct _ e = e
-
-                debug = "BEGIN --\n" ++
-                        "cur: " ++ show (x,y) ++ "\n" ++
-                        "de: " ++ show de ++ "\n" ++
-                        "colPos: " ++ show (x0,y0) ++ "\n" ++
-                        "colRange: " ++ show(rmin, rmax) ++ "\n" ++
-                        "delta: " ++ show delta ++ "\n" ++
-                        "collides: " ++ show collides ++ "\n" ++
-                        "direction: " ++ show d ++ "\nEND"
-
-                catchUp  = trace debug (delta <= 0)
-                eOpt3    = fmap (eventD d) eOpt
-                eOpt4    = fmap (correct d) eOpt3
-                collides = maybe False doesCollides (eOpt3 >>= eventGetRect)
-            setEvent eOpt3
-            when (catchUp || not collides) $ do
-                when catchUp $ do
-                    setEvent eOpt4
-                    setBidon True
-                setCollision Nothing
-
-        noPrevCollision = do
-            setEvent eOpt2
-            when (onEvent && not overlap) onCollisionActivated
-
-    setSelection sOpt2
-    maybe noPrevCollision prevCollision lOpt
     setCursor cOpt
-    when (isJust sOpt || isJust eOpt) draw
+
+  where
+    onSelection r = do
+        (x,y) <- getPointer
+        let action = do
+                x0 <- use rectX
+                y0 <- use rectY
+                rectWidth  .= x-x0
+                rectHeight .= y-y0
+            r1 = execState action r
+        setSelection (Just r1)
+        draw
+
+    onEvent e = do
+        overlap <- isActive Overlap
+        if overlap
+            then overlapMode e
+            else collisionMode e
+
+    overlapMode (Hold r (x,y))     = onHoldOverlapMode r x y
+    overlapMode (Resize r (x,y) a) = onResizeOverlapMode r x y a
+
+    collisionMode (Hold r (x,y))     = onHoldCollisionMode r x y
+    collisionMode (Resize r (x,y) a) = onResizeCollisionMode r x y a
+
+    onHoldOverlapMode r x0 y0 = do
+        (x,y) <- getPointer
+        let r1 = translateRect (x-x0) (y-y0) r
+        setEvent $ Just $ Hold r1 (x,y)
+        draw
+
+    onHoldCollisionMode r x0 y0 = onHoldOverlapMode r x0 y0
+
+    onResizeOverlapMode r x0 y0 a = do
+        (x,y) <- getPointer
+        let r1 = resizeRect (x-x0) (y-y0) a r
+        setEvent $ Just $ Resize r1 (x,y) a
+        draw
+
+    onResizeCollisionMode r x0 y0 a = onResizeOverlapMode r x0 y0 a
 
 onPress :: DhekProgram ()
 onPress = compile $ do
