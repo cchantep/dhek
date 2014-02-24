2fb44766fb223a1b4d7de1c12e51d3bcef59f826
YoEight <yo.eight@gmail.com>
2014-02-24 11:38:47 +0100

    Set up spray.io webapp

---
+++ dhek-integration/build.sbt
@@ -0,0 +1,28 @@
+organization  := "fr.applicius.dhek"
+
+name          := "integration"
+
+version       := "0.1"
+
+scalaVersion  := "2.10.3"
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+resolvers ++= Seq(
+  "spray repo" at "http://repo.spray.io/"
+)
+
+libraryDependencies ++= {
+  val akkaV = "2.2.3"
+  val sprayV = "1.2.0"
+  Seq(
+    "io.spray"            %   "spray-can"     % sprayV,
+    "io.spray"            %   "spray-routing" % sprayV,
+    "io.spray"            %   "spray-testkit" % sprayV  % "test",
+    "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
+    "com.typesafe.akka"   %%  "akka-testkit"  % akkaV   % "test",
+    "org.specs2"          %%  "specs2-core"   % "2.3.7" % "test"
+  )
+}
+
+seq(Revolver.settings: _*)
---
+++ dhek-integration/src/main/scala/dhek/Service.scala
@@ -0,0 +1,40 @@
+package dhek
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+// we don't implement our route structure directly in the service actor because
+// we want to be able to test it independently, without having to spin up an actor
+class ServiceActor extends Actor with Service {
+
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(route)
+}
+
+
+// this trait defines our service behavior independently from the service actor
+trait Service extends HttpService {
+
+  val route =
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Hello Dhek</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
+}
