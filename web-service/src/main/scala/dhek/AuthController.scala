4d9ddfa65a8b60a158c1792ede6385b9f0be9c06
cchantep <chantepie@altern.org>
2014-04-01 15:55:53 +0200

    env.withUsers refactoring

--- web-service/src/main/scala/dhek/AuthController.scala
+++ web-service/src/main/scala/dhek/AuthController.scala
@@ -18,44 +18,37 @@ import resource.managed
 
 case class Auth(email: String, passw: String)
 
-case class AuthController private (secretKey: Array[Char]) {
+final class AuthController private (secretKey: Array[Char]) {
+  def apply(auth: Auth, env: Env) = env.async { complete ⇒
+    env.withUsers.map(_.find(BSONDocument("email" -> auth.email)).
+      one[BSONDocument]).acquireAndGet { ft ⇒
 
-  def apply(auth: Auth, env: Env) {
-    val findToken = env.mongo.map { con ⇒
-      val db = con("dhek")
+      val future: Future[Option[BSONDocument]] =
+        Await.ready(ft, env.settings.timeout)
 
-      db("users").find(BSONDocument("email" -> auth.email)).one[BSONDocument]
-    }
+      env.resp.setContentType("application/json")
 
-    env.async { complete ⇒
-      findToken.acquireAndGet { ft ⇒
-        val future: Future[Option[BSONDocument]] =
-          Await.ready(ft, env.settings.timeout)
+      future.onComplete {
+        case Success(res) ⇒
+          val token: Option[String] = for {
+            doc ← res
+            pwd ← doc.getAs[String]("password") if pwd == sha1Hex(auth.passw)
+            tok ← doc.getAs[String]("adminToken")
+          } yield tok
 
-        env.resp.setContentType("application/json")
+          env.writer.acquireAndGet(_.print(token.fold[String]("null")(tok ⇒
+            ArgJson("token" -> jString(tok)).nospaces)))
 
-        future.onComplete {
-          case Success(res) ⇒
-            val token: Option[String] = for {
-              doc ← res
-              pwd ← doc.getAs[String]("password") if pwd == sha1Hex(auth.passw)
-              tok ← doc.getAs[String]("adminToken")
-            } yield tok
-
-            env.writer.acquireAndGet(_.print(token.fold[String]("null")(tok ⇒
-              ArgJson("token" -> jString(tok)).nospaces)))
-
-            complete()
-          case Failure(e) ⇒
-            e.printStackTrace()
-            env.jsonError(500, e.getMessage)
-            complete()
-        }
+          complete()
+        case Failure(e) ⇒
+          e.printStackTrace()
+          env.jsonError(500, e.getMessage)
+          complete()
       }
     }
   }
 
-  val sha1: Mac = {
+  private val sha1: Mac = {
     val tmp = Mac.getInstance("HmacSHA1")
 
     tmp.init(new SecretKeySpec(Hex.decodeHex(secretKey), "HmacSHA1"))
@@ -64,55 +57,50 @@ case class AuthController private (secretKey: Array[Char]) {
 
   object EmailAlreadyExists extends Exception
 
-  def register(env: Env, email: String, passw: String) {
-    env async { complete ⇒
-      env.withUsers.acquireAndGet { users ⇒
-        def userByEmail: Future[Option[BSONDocument]] =
-          users.find(BSONDocument("email" -> email)).one[BSONDocument]
+  def register(env: Env, email: String, passw: String) = env async { complete ⇒
+    env.withUsers.acquireAndGet { users ⇒
+      def createUser: Future[String] = {
+        val token = Hex.encodeHexString(sha1.doFinal(email.getBytes("UTF-8")))
+        val user = BSONDocument(
+          "email" -> email,
+          "password" -> sha1Hex(passw),
+          "adminToken" -> token
+        )
 
-        def createUser: Future[String] = {
-          val token = Hex.encodeHexString(sha1.doFinal(email.getBytes("UTF-8")))
-          val user = BSONDocument(
-            "email" -> email,
-            "password" -> sha1Hex(passw),
-            "adminToken" -> token
-          )
+        users.insert(user) flatMap {
+          case e if e.inError ⇒ Future.failed(e.fillInStackTrace)
+          case _              ⇒ Future(token)
+        }
+      }
 
-          users.insert(user) flatMap {
-            case e if e.inError ⇒ Future.failed(e.fillInStackTrace)
-            case _              ⇒ Future(token)
+      val token: Future[String] = for {
+        u ← users.find(BSONDocument("email" -> email)).one[BSONDocument]
+        tok ← u.fold(createUser)(_ ⇒ Future.failed(EmailAlreadyExists))
+      } yield tok
+
+      env.resp.setContentType("application/json")
+
+      Await.ready(token, env.settings.timeout) onComplete {
+        case Success(token) ⇒
+          env.writer.acquireAndGet(
+            _.print(ArgJson("token" -> jString(token)).nospaces))
+          complete()
+        case Failure(e) ⇒
+          e match {
+            case EmailAlreadyExists ⇒
+              env.resp.setStatus(400)
+              env.writer.acquireAndGet(
+                _.print(ArgJson("exception" -> jString("Email already exists"))))
+            case e ⇒
+              e.printStackTrace()
+              env.jsonError(500, e.getMessage)
           }
-        }
-
-        val future = for {
-          uOpt ← userByEmail
-          token ← uOpt.fold(createUser)(_ ⇒ Future.failed(EmailAlreadyExists))
-        } yield token
-
-        env.resp.setContentType("application/json")
-
-        Await.ready(future, env.settings.timeout) onComplete {
-          case Success(token) ⇒
-            env.writer.acquireAndGet(
-              _.print(ArgJson("token" -> jString(token)).nospaces))
-            complete()
-          case Failure(e) ⇒
-            e match {
-              case EmailAlreadyExists ⇒
-                env.resp.setStatus(400)
-                env.writer.acquireAndGet(
-                  _.print(ArgJson("exception" -> jString("Email already exists"))))
-              case e ⇒
-                e.printStackTrace()
-                env.jsonError(500, e.getMessage)
-            }
-            complete()
-        }
+          complete()
       }
     }
   }
 }
 
 object AuthController {
-  def make(secretKey: Array[Char]): AuthController = AuthController(secretKey)
+  def make(secretKey: Array[Char]) = new AuthController(secretKey)
 }
