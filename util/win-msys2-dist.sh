71d9cb8d0df79940e2848d0c5a446e82e84d0302
cchantep <chantepie@altern.org>
2014-05-13 15:48:15 +0200

    Release 1.0.5

--- util/win-msys2-dist.sh
+++ util/win-msys2-dist.sh
@@ -5,8 +5,14 @@ OSVER=$1
 VER=`grep '^version:' dhek.cabal | cut -d ':' -f 2 | sed -e 's/^[ ]*//'`
 
 DISTDIR=dist/dhek-win$OSVER-$VER
-mkdir "$DISTDIR"
+mkdir -p "$DISTDIR/bin"
 
-cp dist/build/dhek/dhek "$DISTDIR"
-cd "$DISTDIR"
-ldd dhek.exe | grep '/usr/' | sed -e 's/^.*=>[ \t]*/cp /;s/\.dll.*$/.dll ./' | sh
+cp dist/build/dhek/dhek.exe "$DISTDIR/bin/"
+cp util/win-dlls/*.dll "$DISTDIR/bin/"
+cp -R resources "$DISTDIR/resources"
+
+cat > "$DISTDIR/dhek.bat" << EOF
+start bin\dhek.exe
+EOF
+
+cd dist && zip dhek-win$OSVER-$VER.zip -r dhek-win$OSVER-$VER
